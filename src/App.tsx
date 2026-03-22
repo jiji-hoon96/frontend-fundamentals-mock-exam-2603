6ecc1f2eae7d877909dacebcfa479f3652ff228a
JIHOON LEE <jihoon.lee@hicare.net>
2026-03-22 22:55:14 +0900

    feat: suspense layer 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import normalize from 'emotion-normalize';
-import { css, Global } from '@emotion/react';
-import { useState } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { GlobalPortal } from './GlobalPortal';
+import normalize from "emotion-normalize";
+import { css, Global } from "@emotion/react";
+import { Suspense, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { GlobalPortal } from "./GlobalPortal";
 
-import '_tosslib/sass/app.scss';
-import { PageLayout } from 'pages/PageLayout';
-import { Routes } from 'pages/Routes';
+import "_tosslib/sass/app.scss";
+import { PageLayout } from "pages/PageLayout";
+import { Routes } from "pages/Routes";
 
 export default function App() {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            refetchOnWindowFocus: false,
-            retry: false,
-          },
-        },
-      })
-  );
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						refetchOnWindowFocus: false,
+						retry: false,
+					},
+				},
+			}),
+	);
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <GlobalPortal.Provider>
-        <Global
-          styles={css`
+	return (
+		<QueryClientProvider client={queryClient}>
+			<GlobalPortal.Provider>
+				<Global
+					styles={css`
             ${normalize}
             h1, h2, h3, h4, h5, h6 {
               font-size: 1em;
@@ -33,11 +33,13 @@ export default function App() {
               margin: 0; /* or '0 0 1em' if you're so inclined */
             }
           `}
-        />
-        <PageLayout>
-          <Routes />
-        </PageLayout>
-      </GlobalPortal.Provider>
-    </QueryClientProvider>
-  );
+				/>
+				<PageLayout>
+					<Suspense fallback={<div>Loading...</div>}>
+						<Routes />
+					</Suspense>
+				</PageLayout>
+			</GlobalPortal.Provider>
+		</QueryClientProvider>
+	);
 }
